f7438b015cc952e1767f0f0e2596aee23f1474fd
Ahmad Nassri <ahmad@codeinchaos.com>
2015-07-27 02:01:36 -0400

    test(modifiers): cover invalid & missing modifiers

--- test/modifiers.js
+++ test/modifiers.js
@@ -18,13 +18,23 @@ describe('furmat', function () {
 
       char: function (value) {
         return value.charAt(0)
-      }
+      },
+
+      invalid: 'bar'
     }
   })
 
   describe('modifier', function () {
     it('should use modifier', function () {
       assert.equal(format('%s:upper', 'foo'), 'FOO')
+    })
+
+    it('should skip unknown modifier', function () {
+      assert.equal(format('%s:foo', 'foo'), 'foo:foo')
+    })
+
+    it('should skip invalid modifier', function () {
+      assert.equal(format('%s:foo', 'foo'), 'foo:foo')
     })
 
     it('should chain modifiers', function () {
